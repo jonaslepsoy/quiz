085a197f854b2f83b8e206dd62d540572d7f3464
Jonas Lepsøy <jonas.lepsoy@bouvet.no>
2014-10-25 10:38:27 +0200

    test quiz 2

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 var session = null;
 
 $(document).ready(function () {
+    $('#init').on('click',function(){
+       init($('#appid').val()); 
+    });
+    
+});
+
+function init(appid) {
     var loadCastInterval = setInterval(function () {
-        if (chrome.cast.isAvailable) {
+        if (chrome.cast.isAvailable && appid!='') {
             console.log('Cast has loaded.');
             clearInterval(loadCastInterval);
-            initializeCastApi();
+            initializeCastApi(appid);
         } else {
+            if(appid!='') {console.log('Appid Missing!!!')}
             console.log('Unavailable');
         }
     },
     1000);
-});
+}
 
-function initializeCastApi() {
-    var applicationID = '9F538139';
+function initializeCastApi(appid) {
+    var applicationID = appid;
     var sessionRequest = new chrome.cast.SessionRequest(applicationID);
     var apiConfig = new chrome.cast.ApiConfig(sessionRequest,
     sessionListener,
@@ -23,90 +31,89 @@ function initializeCastApi() {
 };
 
 function sessionListener(e) {
-        session = e;
-        console.log('New session');
-        if (session.media.length != 0) {
-                console.log('Found ' + session.media.length + ' sessions.');
-        }
+    session = e;
+    console.log('New session');
+    if (session.media.length != 0) {
+        console.log('Found ' + session.media.length + ' sessions.');
+    }
 }
 
 function receiverListener(e) {
-        if( e === 'available' ) {
-                console.log("Chromecast was found on the network.");
-        }
-        else {
-                console.log("There are no Chromecasts available.");
-        }
+    if (e === 'available') {
+        console.log("Chromecast was found on the network.");
+    } else {
+        console.log("There are no Chromecasts available.");
+    }
 }
 
 function onInitSuccess() {
-        console.log("Initialization succeeded");
+    console.log("Initialization succeeded");
 }
 
 function onInitError() {
-        console.log("Initialization failed");
+    console.log("Initialization failed");
 }
 
-$('#castme').click(function(){
-        launchApp();
+$('#castme').click(function () {
+    launchApp();
 });
 
 function launchApp() {
-        console.log("Launching the Chromecast App...");
-        chrome.cast.requestSession(onRequestSessionSuccess, onLaunchError);
+    console.log("Launching the Chromecast App...");
+    chrome.cast.requestSession(onRequestSessionSuccess, onLaunchError);
 }
 
 function onRequestSessionSuccess(e) {
-        console.log("Successfully created session: " + e.sessionId);
-        session = e;
+    console.log("Successfully created session: " + e.sessionId);
+    session = e;
 }
 
 function onLaunchError() {
-        console.log("Error connecting to the Chromecast.");
+    console.log("Error connecting to the Chromecast.");
 }
 
 function onRequestSessionSuccess(e) {
-        console.log("Successfully created session: " + e.sessionId);
-        session = e;
-        loadMedia();
+    console.log("Successfully created session: " + e.sessionId);
+    session = e;
+    loadMedia();
 }
 
 function loadMedia() {
-        if (!session) {
-                console.log("No session.");
-                return;
-        }
-
-        var mediaInfo = new
-chrome.cast.media.MediaInfo('http://i.imgur.com/IFD14.jpg');
-        mediaInfo.contentType = 'image/jpg';
-  
-        var request = new chrome.cast.media.LoadRequest(mediaInfo);
-        request.autoplay = true;
-
-        session.loadMedia(request, onLoadSuccess, onLoadError);
+    if (! session) {
+        console.log("No session.");
+        return;
+    }
+    
+    var mediaInfo = new
+    chrome.cast.media.MediaInfo('http://i.imgur.com/IFD14.jpg');
+    mediaInfo.contentType = 'image/jpg';
+    
+    var request = new chrome.cast.media.LoadRequest(mediaInfo);
+    request.autoplay = true;
+    
+    session.loadMedia(request, onLoadSuccess, onLoadError);
 }
 
 function onLoadSuccess() {
-        console.log('Successfully loaded image.');
+    console.log('Successfully loaded image.');
 }
 
 function onLoadError() {
-        console.log('Failed to load image.');
+    console.log('Failed to load image.');
 }
 
-$('#stop').click(function(){
-        stopApp();
+$('#stop').click(function () {
+    stopApp();
 });
 
 function stopApp() {
-        session.stop(onStopAppSuccess, onStopAppError);
+    session.stop(onStopAppSuccess, onStopAppError);
 }
 
 function onStopAppSuccess() {
-        console.log('Successfully stopped app.');
+    console.log('Successfully stopped app.');
 }
 
 function onStopAppError() {
-        console.log('Error stopping app.');
+    console.log('Error stopping app.');
 }
